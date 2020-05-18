8ca61625bec05a9521d20d0961db40ba16d79ea8
Leehosub <hoossky2@gmail.com>
2020-05-18 18:29:01 +0900

    5/18 18:28

---
+++ src/components/common/VuexCounter.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+    <div>카운터: {{count}}</div>
+        <button @click="increment">+</button>
+        <br>
+        <button @click="decrement">-</button>
+        <br>
+        <button @click="incrementIfOdd">Increment If Odd</button>
+        <br>
+        <button @click="incrementAsync">Increment Async Odd</button>
+
+        <div>
+            <div>최근 5개 값의 로그: {{recentHistory}}</div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import {mapGetters, mapActions} from 'vuex' //액션으로 시작 게터로 값을 가져와 끝남
+    export default {
+        computed: mapGetters([ // 자동으로 이루어지는 기능
+            'count',
+            'recentHistory'
+        ]),
+        methods: mapActions([  //내가 선택하는 기능
+            'increment',  //키값
+            'decrement',
+            'incrementIfOdd',
+            'incrementAsync'
+        ])
+    }
+</script>
+
+<style scoped>
+
+</style>
