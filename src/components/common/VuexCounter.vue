93d8fc07f1ae5a9d1ce3c52e71bf75772d692883
Leehosub <hoossky2@gmail.com>
2020-05-19 18:07:17 +0900

    5/19 18:07

--- src/components/common/VuexCounter.vue
+++ src/components/common/VuexCounter.vue
@@ -1,13 +1,13 @@
 <template>
     <div>
-    <div>카운터: {{count}}</div>
-        <button @click="increment">+</button>
+    <div>카운터: {{getCount}}</div>
+        <button @click="increment">더하기</button>
         <br>
-        <button @click="decrement">-</button>
+        <button @click="decrement">빼기</button>
         <br>
-        <button @click="incrementIfOdd">Increment If Odd</button>
+        <button @click="incrementIfOdd">홀수일 때만 증가</button>
         <br>
-        <button @click="incrementAsync">Increment Async Odd</button>
+        <button @click="incrementAsync">1초 후에 자동 증가</button>
 
         <div>
             <div>최근 5개 값의 로그: {{recentHistory}}</div>
@@ -19,7 +19,7 @@
     import {mapGetters, mapActions} from 'vuex' //액션으로 시작 게터로 값을 가져와 끝남
     export default {
         computed: mapGetters([ // 자동으로 이루어지는 기능
-            'count',
+            'getCount',
             'recentHistory'
         ]),
         methods: mapActions([  //내가 선택하는 기능
